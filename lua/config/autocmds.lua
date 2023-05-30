ebc1d8c2b7271f870e9283a6d83bb9f1d8e8f569
sfrick <s.frick@reply.de>
2023-05-30 23:46:55 +0200

    add completion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,18 +12,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
 
 -- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
+vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "<buffer>",
-      once = true,
-      callback = function()
-        vim.cmd(
-          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
-        )
-      end,
-    })
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 })
 
